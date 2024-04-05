2692983d8d96a5802efce36b012dada4288e947e
Maqbool shaikh <maqboolshaikh6666@gmail.com>
2024-04-05 23:53:42 +0530

    modifed mobile about button close

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-// Sticky Navigation Menu JS Code
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
-window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
-    nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
-  }else{
-    nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
-  }
-}
-
 // Side Navigation Menu JS Code
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
+
+// Function to close the side navigation menu
+function closeSideMenu() {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+}
+
+// Event handler for menu button click
 menuBtn.onclick = function(){
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
@@ -25,13 +20,19 @@ menuBtn.onclick = function(){
   body.style.overflow = "hidden";
   scrollBtn.style.pointerEvents = "none";
 }
+
+// Event handler for cancel button click
 cancelBtn.onclick = function(){
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
+  closeSideMenu();
 }
+
+// Event listener for links inside the menu
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    closeSideMenu();
+  });
+});
+
 
 
 
